f9139d1f25a2d35a5c99ba4e0caceee132682605
Tariq <riqprogramming@example.com>
2026-04-15 11:48:07 +0300

    Tariqees full upgrade v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function order(product) {
-    const phone = "2547XXXXXXXX"; // replace with your number
-    const message = `Hello Tariqees, I want to order: ${product}`;
+    const phone = "2547XXXXXXXX"; // your number
+    const message = `Hi Tariqees, I want to order: ${product}`;
     const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
 }
