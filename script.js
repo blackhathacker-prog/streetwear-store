7bb4ac0d9085c95d1269d30cf99674f666736c81
Tariq <riqprogramming@example.com>
2026-04-15 11:35:34 +0300

    upgrade store design

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function order(product) {
-    const phone = "2547XXXXXXXX"; // your number
-    const message = `Hello, I want to order ${product}`;
+    const phone = "2547XXXXXXXX"; // put your number
+    const message = `Hi, I want to order: ${product}`;
     const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
 }
